03dfd3982b84dab3b173f46f3c36fab97d2c54fa
Mark Rödicker <roedicker.external@infineon.com>
2020-11-17 13:33:51 +0100

    Added first example

---
+++ docs/Samples/Console Framework Application/Example-01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using NTrace;
+using NTrace.Services;
+
+namespace Example_01
+{
+  public static class Program
+  {
+    private static ITraceManagementService TraceManagementService;
+    private static ITraceService TraceService;
+
+    public static void Main(string[] args)
+    {
+      BeginApplication();
+
+      ExecuteApplication();
+
+      EndApplication();
+    }
+
+    private static void BeginApplication()
+    {
+      // initialize trace services
+      TraceManagementService = new DefaultTraceManagementService();
+      TraceService = new DefaultTraceService(TraceManagementService);
+
+      // add tracers (e.g. console tracer)
+      TraceManagementService.AddTracer(new ConsoleTracer());
+
+      // configure tracing (e.g. trace all kind of messages)
+      TraceManagementService.Categories = TraceCategories.All;
+    }
+
+    private static void EndApplication()
+    {
+      // signal end of tracing
+      TraceService.EndWrite();
+    }
+
+    private static void ExecuteApplication()
+    {
+      TraceService.BeginMethod();
+
+      try
+      {
+        TraceService.Info("This is the execution of the application");
+      }
+      finally
+      {
+        TraceService.EndMethod();
+      }
+    }
+  }
+}
